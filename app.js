2a57fc968ef275b25513f90987c3e76d77a12d6a
hojokaban <hojokaban@gmail.com>
2020-04-07 23:24:10 +0900

    [Finish]6-5-1

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 $(function(){
-	$('.box1').hide();
-	$('.box1').css({'background-color':'#0000FF'});
+	$('.box1').slideDown(function(){
+		$('.box1').css({
+			'background-color':'#0000FF',
+			'height':'100px'
+	    });
+	});
 });
